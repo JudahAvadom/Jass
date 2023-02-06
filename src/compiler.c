f84abc65a040ffe4b11c7cf6f8fc387d94b1402b
Alberto Casanova <58746126+JudahAvadom@users.noreply.github.com>
2023-02-05 19:40:26 -0500

    Read second argument

---
+++ src/compiler.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "include/compiler.h"
+
+void Compiler(const char* filename){
+    printf("%s \n", filename);
+}
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "include/compiler.h"
 
 int main(int argc, char const *argv[])
 {
@@ -7,5 +8,6 @@ int main(int argc, char const *argv[])
         fprintf(stderr,"\033[1;31mPlease specify input file.\033[0m\n");
         return 1;
     }
+    Compiler(argv[1]);
     return 0;
 }
